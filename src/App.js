723ab4e17afea2795bc85174e2c8bc183548dbce
Rick Andronache <rickandronache@Ricks-MacBook-Pro.local>
2021-11-08 10:56:32 +0200

    update our menu-items using withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 import Homepage from './pages/homepage/homepage.component';
+import './App.css';
+
+const HatsPage = (props) => {
+  return (
+    <div>
+      <h1>Hats page!!!</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
